39dc1727dfb7954c0c5914dd419782ee5a731773
Seongjun Ji <metalg0su@gmail.com>
2019-09-02 17:28:11 +0900

    Add test for timerNew

--- testcase/unittest/baseservice/test_timer_service.py
+++ testcase/unittest/baseservice/test_timer_service.py
@@ -18,11 +18,13 @@
 
 import asyncio
 import datetime
+import functools
 import time
 
 import pytest
 from freezegun import freeze_time
 
+from loopchain.baseservice.timer_new import Timer as TimerNew
 from loopchain.baseservice.timer_service import Timer, TimerService, OffType
 
 attr_start_time = "_Timer__start_time"
@@ -189,3 +191,104 @@ class TestTimerService:
 
         timer_service.stop_timer(timer_key, OffType.normal)
         assert not timer_service.get_timer(timer_key)
+
+
+# ----------
+def blocking_callback(**kwargs):
+    out = f"blocking callback called with: {kwargs}"
+
+    if kwargs.get("exc"):
+        raise RuntimeError(out)
+    else:
+        print(out)
+
+
+async def coro_callback(**kwargs):
+    out = f"coroutine_callback called with: {kwargs}"
+
+    if kwargs.get("exc"):
+        raise RuntimeError(out)
+    else:
+        print(out)
+
+
+@pytest.fixture
+def mock_maker(callback_func, mocker, monkeypatch):
+    def _mock_maker(cb, mocker_fixture, monkeypatch_fixture):
+        callback_func_name = f"{__name__}.{cb.__name__}"
+        print("callback_func_name: ", callback_func_name, "is coro?: ", asyncio.iscoroutinefunction(cb))
+
+        callback = mocker_fixture.MagicMock(callback_func_name, wraps=cb)
+        if asyncio.iscoroutinefunction(cb):
+            def mock_iscoroutinefunction(placeholder):
+                return True
+            monkeypatch_fixture.setattr(asyncio, "iscoroutinefunction", mock_iscoroutinefunction)
+
+        return callback
+
+    return functools.partial(_mock_maker, mocker_fixture=mocker, monkeypatch_fixture=monkeypatch)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("callback_func", [blocking_callback, coro_callback])
+class TestTimerNew:
+    async def test_multiple_timer_runs_in_one_process(self, callback_func):
+        print()
+        print("============")
+        timer1 = TimerNew(callback=callback_func, timeout=2, callback_key="timer1")
+        print("timer1 init")
+        timer2 = TimerNew(callback=callback_func, timeout=3, callback_key="timer2")
+        print("timer2 init")
+
+        start_time = time.time()
+        for s in range(4):
+            curr_time = time.time() - start_time
+            print(">>", s, "초 경과====", curr_time if curr_time > 1 else 0)
+            if s == 1:
+                print("time to kill...")
+                await timer1.stop()
+            await asyncio.sleep(1)
+        print("sleep complete")
+        print(">>", time.time() - start_time, "초 경과====")
+        print("============")
+
+    async def test_start(self, callback_func, mock_maker):
+        callback_func_kwargs = {"callback_kwarg1": "test_value"}
+        mocked_callback = mock_maker(callback_func)
+        timer = TimerNew(callback=mocked_callback, timeout=duration, **callback_func_kwargs)
+
+        assert timer.is_running()
+        assert not mocked_callback.called
+
+        await asyncio.sleep(duration + 0.1)
+        assert not timer.is_running()
+
+    async def test_reset(self, callback_func, mock_maker):
+        timeout = 0.5
+        callback_func_kwargs = {"callback_kwarg1": "test_value"}
+        mocked_callback = mock_maker(callback_func)
+
+        print("========================")
+        print("> initial")
+        timer = TimerNew(callback=mocked_callback, timeout=timeout, **callback_func_kwargs)
+        assert timer.is_running()
+        assert not mocked_callback.called
+
+        print("> before timeout")
+        await asyncio.sleep(timeout - 0.1)
+        assert timer.is_running()
+        assert not mocked_callback.called
+
+        print("> reset timer")
+        await timer.reset()
+
+        print("> before timeout (timeout - 0.1)")
+        await asyncio.sleep(timeout - 0.1)
+        assert timer.is_running()
+        assert not mocked_callback.called
+
+        print("> timeout")
+        await asyncio.sleep(0.2)
+        assert not timer.is_running()
+        assert mocked_callback.called
+        print("========================")

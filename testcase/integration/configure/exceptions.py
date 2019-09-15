08721576afe78747dd6781f49856310bae827ff3
Seongjun Ji <metalg0su@gmail.com>
2019-09-15 18:16:15 +0900

    Configure: Add exceptions

---
+++ testcase/integration/configure/exceptions.py
@@ -0,0 +1,18 @@
+class EmptyAccounts(Exception):
+    pass
+
+
+class NotFoundChannelConfig(Exception):
+    pass
+
+
+class NotEnoughAccounts(Exception):
+    pass
+
+
+class DuplicatedChannelConfig(Exception):
+    pass
+
+
+class NotFoundChannelManageDataPath(Exception):
+    pass

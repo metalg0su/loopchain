2e7c23936c2728f06867828a85e650178c2d13b3
Seongjun Ji <metalg0su@gmail.com>
2020-06-01 13:41:38 +0900

    Prep

--- loopchain/blockchain/peer_loader.py
+++ loopchain/blockchain/peer_loader.py
@@ -11,6 +11,35 @@ from loopchain.blockchain.types import ExternalAddress, Hash32
 from loopchain.channel.channel_property import ChannelProperty
 
 
+class Prep:
+    def __init__(self, address: ExternalAddress, endpoint: str):
+        self._address: ExternalAddress = address
+        self._endpoint: str = endpoint
+
+    @property
+    def address(self) -> ExternalAddress:
+        return self._address
+
+    @property
+    def endpoint(self) -> str:
+        return self._endpoint
+
+    @classmethod
+    def from_(cls, data: dict):
+        address = data.get("id") or data.get("address")
+        address = ExternalAddress.fromhex_address(address)
+
+        endpoint = data.get("p2pEndpoint") or data.get("peer_target")
+
+        return cls(address=address, endpoint=endpoint)
+
+    def serialize(self) -> dict:
+        return {
+            "id": self._address.hex_hx(),
+            "p2pEndpoint": self._endpoint
+        }
+
+
 class PeerLoader:
     def __init__(self):
         pass
@@ -34,10 +63,7 @@ class PeerLoader:
 
     @staticmethod
     def _get_peer_root_hash(peers: list):
-        block_prover = BlockProver(
-            (ExternalAddress.fromhex_address(peer['id']).extend() for peer in peers),
-            BlockProverType.Rep
-        )
+        block_prover = BlockProver((peer.address.extend() for peer in peers), BlockProverType.Rep)
 
         return block_prover.get_proof_root()
 
@@ -46,13 +72,15 @@ class PeerLoader:
         blockchain = ObjectManager().channel_service.block_manager.blockchain
         last_block = blockchain.last_block
         rep_root_hash = (last_block.header.reps_hash if last_block else Hash32.fromhex(reps_hash))
-        return blockchain.find_preps_by_roothash(rep_root_hash)
+        reps = blockchain.find_preps_by_roothash(rep_root_hash)
+
+        return [Prep.from_(rep) for rep in reps]
 
     @staticmethod
     def _load_peers_from_file():
         channel_info = utils.load_json_data(conf.CHANNEL_MANAGE_DATA_PATH)
         reps: list = channel_info[ChannelProperty().name].get("peers")
-        return [{"id": rep["id"], "p2pEndpoint": rep["peer_target"]} for rep in reps]
+        return [Prep.from_(rep) for rep in reps]
 
     @staticmethod
     def _load_peers_from_rest_call(crep_root_hash: str):
@@ -61,4 +89,4 @@ class PeerLoader:
             RestMethod.GetReps,
             RestMethod.GetReps.value.params(crep_root_hash)
         )
-        return [{"id": rep["address"], "p2pEndpoint": rep["p2pEndpoint"]} for rep in reps]
+        return [Prep.from_(rep) for rep in reps]

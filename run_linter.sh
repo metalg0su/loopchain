28189dab5341ea649fd62f2f8dc3beb18404fe2b
Seongjun Ji <metalg0su@gmail.com>
2020-09-09 19:20:48 +0900

    ffd

---
+++ run_linter.sh
@@ -0,0 +1,3 @@
+curl https://gist.githubusercontent.com/metalg0su/73a8a7cfed9554ed3f8d9d56c26290f4/raw/ccbcde127a7e0f2f2e4d715849d92f6f2a869d24/setup.cfg > setup.cfg
+
+flake8 loopchain/channel/channel_state*
